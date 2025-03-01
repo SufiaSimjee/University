086a12e3bd0a9f52ccfcdcf980c29deb4742b3a4
Sufia Yusoof Simjee <sufiasimjee123@gmail.com>
2025-03-01 08:05:37 +0630

    New Commit

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,8 @@
-import express from 'express';
-import connectDB from './config/db.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import userRoutes from './routes/userRoutes.js';
-import departmentRoutes from './routes/departmentRoutes.js';
-
-dotenv.config();
-const port = process.env.PORT || 6000;
-
-// Connect to MongoDB
-connectDB();
-
+const express = require("express");
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('Server is running');
-});
-app.use('/api/users', userRoutes);
-app.use('/api/departments', departmentRoutes);
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-
-app.use(notFound);
-app.use(errorHandler);
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
