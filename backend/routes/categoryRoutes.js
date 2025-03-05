f532dbed3e5180a997e5720bfa5f6430c738001f
Sufia Yusoof Simjee <sufiasimjee123@gmail.com>
2025-03-05 10:04:28 +0630

    Category Controller

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -7,7 +7,7 @@ import {
 
 const router = express.Router();
 
-routerter.route('/').get(getAllCategories);
+router.route('/').get(getAllCategories);
 router.route('/create').post(protect, roleAccess(['Admin', 'QA Manager']),  createCategory);
 
 export default router;
