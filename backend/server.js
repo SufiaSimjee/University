fc317c3704df6a6f4a504a0adf7c63179e2f4daf
Sufia Yusoof Simjee <sufiasimjee123@gmail.com>
2025-03-07 19:36:33 +0630

    New

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,6 @@ const allowCors = fn => async (req, res) => {
     res.status(200).end();
     return;
   }
-
-  
   return await fn(req, res);
 };
 
@@ -38,15 +36,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-const allowedOrigin = '*'; 
-app.use(cors({
-  origin: allowedOrigin,     
-  credentials: true,          
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-  allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Content-Type', 'Authorization'] 
-}));
-
+app.use(allowCors);
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/roles', roleRoutes);
