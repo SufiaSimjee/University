ab8ea87e53839c7c8056f94105feca1dc0004ce4
Sufia Yusoof Simjee <sufiasimjee123@gmail.com>
2025-03-07 18:06:20 +0630

    New

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -16,7 +16,7 @@ const generateToken = (res , userId) => {
          httpOnly : true,
          secure: process.env.NODE_ENV === 'production',
          sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Strict',
-         
+         domain: process.env.NODE_ENV === 'production' ? 'university-red.vercel.app' : 'localhost: 7000',
           maxAge : 30 * 24 * 60 * 60 * 1000
       });
  };
