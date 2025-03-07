a27d10446eb57a12faf412ad62f4f7c5c1456f62
Sufia Yusoof Simjee <sufiasimjee123@gmail.com>
2025-03-07 15:23:15 +0630

    New

--- backend/server.js
+++ backend/server.js
@@ -45,21 +45,6 @@ const app = express();
 //   })
 // );
 
-// Middleware
-app.use(
-  cors({
-    origin: 'https://university-frontend-six.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-      'X-CSRF-Token',
-      'X-Requested-With',
-      'Accept',
-      'Content-Type',
-      'Authorization',
-    ],
-    credentials: true,
-  })
-);
 
 app.use(cookieParser());
 app.use(express.json());
