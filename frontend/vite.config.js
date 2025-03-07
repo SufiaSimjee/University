21e4f1d388b07e166ec7b9e3191ebcc20aef9a39
Sufia Yusoof Simjee <sufiasimjee123@gmail.com>
2025-03-07 15:33:36 +0630

    New

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'https://university-rose.vercel.app',
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   }
